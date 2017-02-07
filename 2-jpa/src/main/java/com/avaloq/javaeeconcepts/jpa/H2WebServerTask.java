de386d23346ccdd4118928878becec1bba46d4e9
Simon Nattrass <simon.nattrass@avaloq.com>
2017-02-07 15:59:33 +0000

    Move H2 WebServer into it's own thread (omitted from previous commit)

---
+++ 2-jpa/src/main/java/com/avaloq/javaeeconcepts/jpa/H2WebServerTask.java
@@ -0,0 +1,32 @@
+package com.avaloq.javaeeconcepts.jpa;
+
+import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
+
+import org.h2.tools.Server;
+
+public class H2WebServerTask implements Runnable {
+
+  @Override
+  public void run () {
+    Server webServer = null;
+    Server server = null;
+
+    try {
+      webServer = Server.createWebServer("-webAllowOthers", "-webPort", "8082").start();
+      server = Server.createTcpServer("-tcpAllowOthers", "-tcpPort", "9092").start();
+      TimeUnit.MINUTES.sleep(5);
+    }
+    catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    finally {
+      if (webServer != null && server != null) {
+        webServer.stop();
+        server.stop();
+      }
+    }
+  }
+}
