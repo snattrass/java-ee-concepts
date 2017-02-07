176ea685f74f0f608d9b0b7a644301641920329c
Simon Nattrass <simon.nattrass@avaloq.com>
2017-02-07 16:01:21 +0000

    Stop generating the SQL for create/drop

--- create.sql
+++
@@ -1,5 +0,0 @@
-create sequence hibernate_sequence start with 1 increment by 1
-create table authors (id bigint not null, name varchar(255), primary key (id))
-create table books (id bigint not null, description varchar(255), price float, title varchar(255), author_id bigint, primary key (id))
-create table topics (id bigint not null, title varchar(255), primary key (id))
-alter table books add constraint FKfjixh2vym2cvfj3ufxj91jem7 foreign key (author_id) references authors
--- drop.sql
+++
@@ -1,4 +0,0 @@
-drop table authors if exists
-drop table books if exists
-drop table topics if exists
-drop sequence if exists hibernate_sequence
