2fe1b27ffc87aa7687cebbd635d78750a5cc346c
Simon Nattrass <simon.nattrass@avaloq.com>
2017-02-10 11:06:56 +0000

    Tidying comments

--- 4-jms-client/src/main/java/com/avaloq/javaeeconcepts/client/BoodOrderProducer.java
+++ 4-jms-client/src/main/java/com/avaloq/javaeeconcepts/client/BoodOrderProducer.java
@@ -15,18 +15,17 @@ import com.avaloq.javaeeconcepts.to.BookOrderTO;
 public class BoodOrderProducer {
 
   public static void main(String[] args) throws NamingException, IOException {
-    // Creates an orderDto with a total amount parameter
     BookOrderTO bookOrder = new BookOrderTO(123L, new Date(), "Simon Nattrass", 23.00F);
 
-    // Gets the JNDI context
+    // grab the JNDI context
     Context jndiContext = Environment.getContext();
 
-    // Looks up the administered objects
+    // lookup the administered objects
     ConnectionFactory connectionFactory = (ConnectionFactory) jndiContext.lookup("jms/RemoteConnectionFactory");
     Destination queue = (Destination) jndiContext.lookup("jms/javaee-concepts/queue/BookOrderQueue");
 
     try (JMSContext jmsContext = connectionFactory.createContext("jms-user", "novell")) {
-      // Sends an object message to the queue
+      // send message to the queue
       JMSProducer producer = jmsContext.createProducer();
       producer.send(queue, bookOrder);
       System.out.println("Order sent : " + bookOrder.toString());
