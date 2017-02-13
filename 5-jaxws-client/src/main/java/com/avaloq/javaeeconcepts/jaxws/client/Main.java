08aaca21801e82b64c62d153ea994e9d2520756e
Simon Nattrass <simon.nattrass@avaloq.com>
2017-02-13 17:03:31 +0000

    Converting LocalDate to XMLGregorianCalendar

--- 5-jaxws-client/src/main/java/com/avaloq/javaeeconcepts/jaxws/client/Main.java
+++ 5-jaxws-client/src/main/java/com/avaloq/javaeeconcepts/jaxws/client/Main.java
@@ -1,18 +1,32 @@
 package com.avaloq.javaeeconcepts.jaxws.client;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.GregorianCalendar;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
 import com.avaloq.javaeeconcepts.ws.CardValidator;
 import com.avaloq.javaeeconcepts.ws.CardValidatorService;
 import com.avaloq.javaeeconcepts.ws.CreditCardTO;
 
 public class Main {
-  public static void main (String[] args) {
+  public static void main (String[] args) throws  Exception {
     CardValidator cardValidator = new CardValidatorService().getCardValidatorPort();
 
     CreditCardTO creditCardTO = new CreditCardTO();
     creditCardTO.setType("Amex");
-    creditCardTO.setExpiryDate("foo");
+    creditCardTO.setExpiryDate(oneYearFromNow());
     creditCardTO.setNumber("1234");
 
     System.out.println("Result: " + cardValidator.validate(creditCardTO));
+  }
+
+  private static XMLGregorianCalendar oneYearFromNow() throws DatatypeConfigurationException {
+    LocalDate oneYearFromNow = LocalDate.now().plusYears(1);
+    GregorianCalendar gcal = GregorianCalendar.from(oneYearFromNow.atStartOfDay(ZoneId.systemDefault()));
+    return DatatypeFactory.newInstance().newXMLGregorianCalendar(gcal);
   }
 }
