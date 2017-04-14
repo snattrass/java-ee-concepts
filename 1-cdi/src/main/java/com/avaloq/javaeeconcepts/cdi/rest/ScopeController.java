4faa3644d812d2f6caa7aa3a2b5464affe3e82c7
Simon Nattrass <simon.nattrass@avaloq.com>
2017-04-14 11:06:24 +0100

    Commenting out ScopeController to start with

--- 1-cdi/src/main/java/com/avaloq/javaeeconcepts/cdi/rest/ScopeController.java
+++ 1-cdi/src/main/java/com/avaloq/javaeeconcepts/cdi/rest/ScopeController.java
@@ -1,32 +1,32 @@
-package com.avaloq.javaeeconcepts.cdi.rest;
-
-import javax.ejb.Stateless;
-import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-
-import com.avaloq.javaeeconcepts.cdi.scopes.ApplicationScopedBean;
-import com.avaloq.javaeeconcepts.cdi.scopes.RequestScopedBean;
-
-@Stateless
-@Path("/scope")
-public class ScopeController {
-
-  @Inject
-  private RequestScopedBean requestScopedBean;
-
-  @Inject
-  private ApplicationScopedBean applicationScopedBean;
-
-  @GET
-  @Path("/request")
-  public void runRequestScopedBean() {
-    requestScopedBean.foo();
-  }
-
-  @GET
-  @Path("/application")
-  public void runApplicationScopedBean() {
-    applicationScopedBean.foo();
-  }
-}
+//package com.avaloq.javaeeconcepts.cdi.rest;
+//
+//import javax.ejb.Stateless;
+//import javax.inject.Inject;
+//import javax.ws.rs.GET;
+//import javax.ws.rs.Path;
+//
+//import com.avaloq.javaeeconcepts.cdi.scopes.ApplicationScopedBean;
+//import com.avaloq.javaeeconcepts.cdi.scopes.RequestScopedBean;
+//
+//@Stateless
+//@Path("/scope")
+//public class ScopeController {
+//
+//  @Inject
+//  private RequestScopedBean requestScopedBean;
+//
+//  @Inject
+//  private ApplicationScopedBean applicationScopedBean;
+//
+//  @GET
+//  @Path("/request")
+//  public void runRequestScopedBean() {
+//    requestScopedBean.foo();
+//  }
+//
+//  @GET
+//  @Path("/application")
+//  public void runApplicationScopedBean() {
+//    applicationScopedBean.foo();
+//  }
+//}
